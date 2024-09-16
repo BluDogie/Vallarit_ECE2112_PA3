d49b4f05395f728094dc2679c78a3c3a937f3490
BluDogie <marickandrei@yahoo.com>
2024-09-16 12:04:51 +0800

    Updated Vallarit_Pandas-P1.py
    Wrong code, fixed the output for problem #1

--- Vallarit_Pandas-P1.py
+++ Vallarit_Pandas-P1.py
@@ -3,12 +3,8 @@
 
 #a. Load the corresponding .csv file into a data frame named cars using pandas
 #b. Display the first five and last five rows of the resulting cars.
-
 import pandas as pd #import pandas library
 
 cars = pd.read_csv("cars.csv") #load the .csv file into a data frame named cars using pandas
-
-# For Letter A
-cars.iloc[:,::2].head() # Use iloc to specifically get which columns to display, specifically the columns that are odd-numbered.
-# : to display all rows
-# ::2 to start at the beginning and step by 2.
+p1 = pd.concat([cars.head(), cars.tail()]) #display the first 5 rows and last 5 rows of cars.csv
+p1
